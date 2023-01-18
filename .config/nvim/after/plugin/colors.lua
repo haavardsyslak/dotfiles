9e39eba96c4b5b7a9d8402a84076cee0c5a09ed3
Håvard Syslak <haavard.syslak@gmail.com>
2023-01-18 17:27:05 +0100

    Colors

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,8 +1,8 @@
 function Fargelegg(color)
-	color = color or "onedark"
+	color = color or "one_monokai"
 	vim.cmd.colorscheme(color)
-	vim.api.nvim_set_hl(0, "normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 end
 -- italics seem to be working
