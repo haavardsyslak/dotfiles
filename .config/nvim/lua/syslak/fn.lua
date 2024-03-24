8b0f0d78e46e455fa3a12241c0fea90ee79ee6e3
Håvard Syslak <haavard.syslak@gmail.com>
2024-03-24 16:10:43 +0100

    nvim

--- .config/nvim/lua/syslak/fn.lua
+++ .config/nvim/lua/syslak/fn.lua
@@ -1,11 +1,11 @@
 
 function pdf_open()
-   local bufname = vim.fn.expand("%:t:r") 
+   local bufname = vim.fn.expand("%:t:r")
 
    local pdfpath = vim.fn.findfile(bufname .. ".pdf")
 
    if pdfpath ~= "" then
-        vim.fn.system(string.format("zathura './%s'", pdfpath))
+        vim.fn.system(string.format("zathura './%s' &disown", pdfpath))
 
     else
         vim.notify("file not found: " .. bufname .. ".pdf")
