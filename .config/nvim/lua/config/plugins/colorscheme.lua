0dcdeb8b7d5ba69d1555e8876fd378cb70aa8249
haavardsyslak <haavard.syslak@gmail.com>
2025-08-09 10:19:01 +0200

    make floats slightly darker

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -7,6 +7,9 @@
             require("gruvbox").setup({
                 transparent_mode = true,
                 dim_inactive = false,
+				overrides = {
+					NormalFloat = {bg = "#1d2025" },
+				}
             })
             vim.cmd.colorscheme("gruvbox")
         end
