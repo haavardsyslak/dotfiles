d366ba4461db8a476fb7a075a79310c6360a9392
haavardsyslak <haavard.syslak@gmail.com>
2025-08-10 17:27:49 +0200

    fall back to osc52 clipboard

--- .config/nvim/plugin/clipboard.lua
+++ .config/nvim/plugin/clipboard.lua
@@ -2,13 +2,19 @@ if vim.fn.has("wsl") == 1 then
 	vim.g.clipboard = {
 		name = "WslClipboard",
 		copy = {
-		["+"] = "clip.exe",
-		["*"] = "clip.exe", 
-	}, 
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
 		paste = {
 			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
 			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-	},
-	cache_enabled = 0,
-}
+		},
+		cache_enabled = 0,
+	}
+
+
+-- TODO: Only fallback to osc52 if we are running in docker
+elseif vim.fn.executable("wl-copy") == 0 or vim.fn.executable("wl-paste") == 0 then
+	vim.g.clipboard = "osc52"
 end
+
