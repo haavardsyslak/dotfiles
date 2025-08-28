c54803d6b4ef98d8fd14e7bf3d69a1627c59236e
haavardsyslak <haavard.syslak@gmail.com>
2025-08-29 00:15:33 +0200

    home manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -42,6 +42,7 @@
 	  pkgs.less
 	  pkgs.bat
 	  pkgs.cloc
+	  pkgs.tree
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
