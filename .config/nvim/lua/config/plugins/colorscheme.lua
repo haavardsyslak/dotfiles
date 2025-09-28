7fc0af1b1d255cfeb015ff29620835a176768e59
haavardsyslak <haavard.syslak@gmail.com>
2025-09-28 17:42:49 +0200

    nvim update

--- .config/nvim/lua/config/plugins/colorscheme.lua
+++ .config/nvim/lua/config/plugins/colorscheme.lua
@@ -1,17 +1,18 @@
-    return {
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = true,
-        init = function()
-            require("gruvbox").setup({
-                transparent_mode = true,
-                dim_inactive = false,
-				overrides = {
-					NormalFloat = {bg = "#1d2025" },
-				}
-            })
-            vim.cmd.colorscheme("gruvbox")
-        end
-    },
-    }
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    init = function()
+      require("gruvbox").setup({
+        transparent_mode = true,
+        dim_inactive = false,
+        inverse = false,
+        overrides = {
+          NormalFloat = { bg = "#1d2025" },
+        }
+      })
+      vim.cmd.colorscheme("gruvbox")
+    end
+  },
+}
