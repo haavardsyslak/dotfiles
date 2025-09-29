e10dc138d340e24c7dfee4e842d38e409cabf7ce
haavardsyslak <haavard.syslak@gmail.com>
2025-09-29 08:42:45 +0200

    formatting

---
+++ .config/nvim/lua/config/plugins/formatting.lua
@@ -0,0 +1,21 @@
+return {
+  'stevearc/conform.nvim',
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua", lsp_format = "fallback" },
+        -- Conform will run multiple formatters sequentially
+        python = { "black", lsp_format = "fallback" },
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        cpp = { "clang-format" },
+      },
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+      end,
+    })
+  end,
+}
